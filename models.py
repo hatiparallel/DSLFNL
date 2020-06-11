fe58c586e53f73aa4a329a1513fbc1094aec26ae
hatiparallel <pneumoulramicro3.7@gmail.com>
2020-06-11 14:44:29 +0900

    output features in ResNet

--- models.py
+++ models.py
@@ -41,7 +41,7 @@ class ResNet(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
         x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        y = self.output(x)
+        f = torch.flatten(x, 1)
+        y = self.output(f)
 
-        return y
+        return f, y

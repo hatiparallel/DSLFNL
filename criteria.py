9ed003d0b908fbe5b8c0072f7d7b164c84e112b9
hatiparallel <pneumoulramicro3.7@gmail.com>
2020-06-11 14:22:03 +0900

    add NoisyCCE

--- criteria.py
+++ criteria.py
@@ -1,2 +1,14 @@
 import numpy as np
 import torch
+
+class NoisyCrossEntropy(nn.Module):
+    def __init__(self, alpha):
+        super(NoisyCrossEntropy, self).__init__()
+        self.alpha = alpha
+        self.cce = nn.CrossEntropyLoss()
+        return
+
+    def forward(self, pred, target, target_modified):
+        loss = self.cce(pred, target)
+        loss_modified = self.cce(pred, target_modified)
+        return self.alpha*loss + self.alpha*loss_modified
